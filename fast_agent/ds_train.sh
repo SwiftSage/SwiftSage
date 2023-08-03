7f4f185f27d8b147b23adf5d42fe0e6310c45235
(Bill) Yuchen Lin <yuchen.lin@usc.edu>
2023-08-03 13:22:41 -0700

    Update ds_train.sh

--- fast_agent/ds_train.sh
+++ fast_agent/ds_train.sh
@@ -2,17 +2,17 @@ USE_TF=0
 
 deepspeed --master_port 29510 \
 		./ds_train.py \
-		--cache_dir /net/nfs/path/to/cache/ \
+	--cache_dir /net/nfs/path/to/cache/ \
         --model_name_or_path google/flan-t5-large \
         --output_dir model_ckpts/flan_large_0413 \
         --do_train \
-		--do_eval \
-		--save_total_limit=100 \
+	--do_eval \
+	--save_total_limit=100 \
         --train_file ../data_utils/data_v5/fast_system.train.jsonl \
-		--validation_file ../data_utils/data_v5/fast_system.val.jsonl \
-		--predict_with_generate 0 \
+	--validation_file ../data_utils/data_v5/fast_system.val.jsonl \
+	--predict_with_generate 0 \
         --learning_rate 1e-4 \
-		--adam_eps 1e-06 \
+	--adam_eps 1e-06 \
         --overwrite_output_dir \
         --max_source_length 1024 \
         --max_target_length 16 \
