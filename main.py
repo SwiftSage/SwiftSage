efaa9bad35473fcb4dc806d26f5a2268086f77d2
yifan <yfsong@pku.edu.cn>
2024-09-21 23:48:34 +0800

    add benchmark data load

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import os
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from abc import ABC, abstractmethod
-import openai
-import json 
-import logging
+import argparse
 import datetime
+import json
+import logging
+import multiprocessing
+import os
 import re
-import hjson
-import multiprocessing 
+from abc import ABC, abstractmethod
 
-from utils import extract_and_parse_markup, Agent, api_configs, LLMClient, PromptTemplate, setup_logging
+import hjson
+import numpy as np
+import openai
+from sklearn.metrics.pairwise import cosine_similarity
+
+from data_loader import load_data
 from code_executor import PythonExecutor
+from utils import (Agent, LLMClient, PromptTemplate, api_configs,
+                   extract_and_parse_markup, setup_logging)
+
 
 logger = setup_logging()
 
@@ -276,6 +281,7 @@ class SwiftSage:
         logger.info("Problem solving completed")
         return current_reasoning, current_solution
 
+
 def run_test(swiftsage, problem, max_iterations=5, reward_threshold=8):
     logger.info(f"Testing problem: {problem}")
     reasoning, solution = swiftsage.solve(problem, max_iterations, reward_threshold)
@@ -284,8 +290,12 @@ def run_test(swiftsage, problem, max_iterations=5, reward_threshold=8):
     logger.info("=" * 50)
 
 
-def main():
+def run_benchmark(swiftsage, args, max_iterations=5, reward_threshold=8):
+    examples = load_data(args.dataset_names, args.split, args.data_dir, args.num_test_sample, args.shuffle)
+    pass    # TODO: benchmark evaluation
 
+
+def main(args):
 
     # TODO: for retrieval augmentation (not implemented yet now) 
     # dataset = ["Example problem 1: ...", "Example problem 2: ...", "Example problem 3: ..."]
@@ -309,66 +319,99 @@ def main():
     # }
 
     swift_config = {
-        "model_id": "meta-llama/Meta-Llama-3-8B-Instruct-Turbo",
-        "api_config": api_configs['Together']
+        "model_id": args.swift_model_id,
+        "api_config": api_configs[args.api_provider]
     }
 
     reward_config = {
-        "model_id": "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
-        "api_config": api_configs['Together']
+        "model_id": args.reward_model_id,
+        "api_config": api_configs[args.api_provider]
     }
 
     sage_config = {
-        "model_id": "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
-        "api_config": api_configs['Together']
+        "model_id": args.sage_model_id,
+        "api_config": api_configs[args.api_provider]
     }
 
     # specify the path to the prompt templates
-    prompt_template_dir = "./prompt_templates"
+    prompt_template_dir = args.prompt_template_dir
     dataset = [] 
     embeddings = [] # TODO: for retrieval augmentation (not implemented yet now)
     s2 = SwiftSage(
-        dataset, 
-        embeddings, 
-        prompt_template_dir, 
+        dataset,
+        embeddings,
+        prompt_template_dir,
         swift_config,
         sage_config,
         reward_config,
-        use_retrieval=False,
-        start_with_sage=False
+        use_retrieval=args.use_retrieval,
+        start_with_sage=args.start_with_sage,
     )
 
- 
-    test_problems = [
-        "Solve the equation: 2x + 5 = 13", # 0
-        "If h(x)=x-4 and g(h(x))=x^2-8x+10, find g(x)? show the formula for g(x)", # 1
-        "Solve the equation: 6y + 5 = 29", # 2
-        "Who lives longer, Lowell Sherman or Jonathan Kaplan?", # 3
-        "9.9 or 9.11 --  which is bigger?", # 4
-        "How can you solve the quadratic equation 3x^2 + 7x + 4 = 0 using the quadratic formula?", # 5
-        "Explain why sound waves cannot travel in a vacuum?", # 6
-        "How many grams of hydrogen (H) are present in 23.5 grams of water (H2O)?", # 7
-        "What is the distance between the points (2, 3) and (5, 8)?", # 8
-        "Why can the Hubble telescope capture clear images of distant stars and galaxies, but not a detailed image of Pluto?", # 9
-        """A rectangular band formation is a formation with $m$ band members in each of $r$ rows, where $m$ and $r$ are integers. A particular band has less than 100 band members. The director arranges them in a rectangular formation and finds that he has two members left over. If he increases the number of members in each row by 1 and reduces the number of rows by 2, there are exactly enough places in the new formation for each band member. What is the largest number of members the band could have?""",
-        """Tim wants to invest some money in a bank which compounds quarterly with an annual interest rate of $7\%$. To the nearest dollar, how much money should he invest if he wants a total of $\$60,\!000$ at the end of $5$ years?""",
-        """In an SR latch built from NOR gates, which condition is not allowed
+    if args.eval_mode == "test":
+        test_problems = [
+            "Solve the equation: 2x + 5 = 13", # 0
+            "If h(x)=x-4 and g(h(x))=x^2-8x+10, find g(x)? show the formula for g(x)", # 1
+            "Solve the equation: 6y + 5 = 29", # 2
+            "Who lives longer, Lowell Sherman or Jonathan Kaplan?", # 3
+            "9.9 or 9.11 --  which is bigger?", # 4
+            "How can you solve the quadratic equation 3x^2 + 7x + 4 = 0 using the quadratic formula?", # 5
+            "Explain why sound waves cannot travel in a vacuum?", # 6
+            "How many grams of hydrogen (H) are present in 23.5 grams of water (H2O)?", # 7
+            "What is the distance between the points (2, 3) and (5, 8)?", # 8
+            "Why can the Hubble telescope capture clear images of distant stars and galaxies, but not a detailed image of Pluto?", # 9
+            """A rectangular band formation is a formation with $m$ band members in each of $r$ rows, where $m$ and $r$ are integers. A particular band has less than 100 band members. The director arranges them in a rectangular formation and finds that he has two members left over. If he increases the number of members in each row by 1 and reduces the number of rows by 2, there are exactly enough places in the new formation for each band member. What is the largest number of members the band could have?""",
+            """Tim wants to invest some money in a bank which compounds quarterly with an annual interest rate of $7\%$. To the nearest dollar, how much money should he invest if he wants a total of $\$60,\!000$ at the end of $5$ years?""",
+            """In an SR latch built from NOR gates, which condition is not allowed
 
-        Options:
-        [ "S=0, R=2", "S=2, R=2", "S=1, R=1", "S=1, R=-1", "S=1, R=2", "S=0, R=0", "S=2, R=0", "S=1, R=0", "S=2, R=1", "S=0, R=1" ]
+            Options:
+            [ "S=0, R=2", "S=2, R=2", "S=1, R=1", "S=1, R=-1", "S=1, R=2", "S=0, R=0", "S=2, R=0", "S=1, R=0", "S=2, R=1", "S=0, R=1" ]
 
-        Which one is the correct answer?""",
-        # ... add other problems here ...
-        """How many letter r are there in the word "strawberry"?"""
-    ]
+            Which one is the correct answer?""",
+            # ... add other problems here ...
+            """How many letter r are there in the word "strawberry"?"""
+        ]
 
-    # for problem in test_problems:
-    pid = 7
-    print(f"Problem {pid}: {test_problems[pid]}")
-    run_test(s2, test_problems[pid])
-
+        # for problem in test_problems:
+        pid = 7
+        print(f"Problem {pid}: {test_problems[pid]}")
+        run_test(s2, test_problems[pid], args.max_iterations, args.reward_threshold)
+    elif args.eval_mode == "benchmark":
+        run_benchmark(s2, args, args.max_iterations, args.reward_threshold)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--eval_mode", default="test", choices=["test", "benchmark"], type=str)
+
+    parser.add_argument("--dataset_names", default="MATH", type=str)
+    parser.add_argument("--data_dir", default="./data", type=str)
+    parser.add_argument("--split", default="test", type=str)
+    parser.add_argument("--num_test_sample", default=-1, type=int)  # -1 for full data
+    parser.add_argument("--shuffle", action="store_true")
+
+    parser.add_argument("--api_provider", default="Together", choices=["Together", "SambaNova"], type=str)
+    parser.add_argument("--swift_model_id", default="meta-llama/Meta-Llama-3-8B-Instruct-Turbo", type=str)
+    parser.add_argument("--reward_model_id", default="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo", type=str)
+    parser.add_argument("--sage_model_id", default="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo", type=str)
+
+    parser.add_argument("--prompt_template_dir", default='./prompt_templates', type=str)
+    parser.add_argument("--use_retrieval", action="store_true")
+    parser.add_argument("--start_with_sage", action="store_true")
+
+    parser.add_argument("--max_iterations", default=5, type=int)
+    parser.add_argument("--reward_threshold", default=8, type=int)
+
+    parser.add_argument("--save_outputs", action="store_true")
+    parser.add_argument("--output_dir", default="./output", type=str)
+    parser.add_argument("--overwrite", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.api_provider == "SambaNova":
+        args.swift_model_id = args.swift_model_id.split("/")[-1][:-len("Turbo")]
+        args.reward_model_id = args.reward_model_id.split("/")[-1][:-len("Turbo")]
+        args.sage_model_id = args.sage_model_id.split("/")[-1][:-len("Turbo")]
+
     multiprocessing.set_start_method('spawn')
-    main()
+    main(args)
